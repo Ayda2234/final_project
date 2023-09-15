fb7e53e746738a53e6504742573f2a53f32b8670
Ayda2234 <202032009633@etu.univ-chlef.dz>
2023-09-15 15:22:18 +0200

    commit now

--- scripts.js
+++ scripts.js
@@ -1,39 +1,87 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const messageInput = document.getElementById('message-input');
-    const sendButton = document.getElementById('send-button');
-    const chatMessages = document.querySelector('.chat-messages');
+const webTrainingLink = document.getElementById("webTrainingLink");
+document.addEventListener("DOMContentLoaded", function () {
+    const messageInput = document.getElementById("message-inputs"); // Updated ID here
 
-    // Function to add a new message to the chat
-    function addMessage(message) {
-        const messageElement = document.createElement('div');
-        messageElement.classList.add('message');
-        messageElement.innerText = message;
-        chatMessages.appendChild(messageElement);
-        
-        // Scroll to the latest message
-        chatMessages.scrollTop = chatMessages.scrollHeight;
-    }
+    if (messageInput) {
+        const showMessageInputButton = document.getElementById("show-message-inputs");
 
-    // Event listener for sending a message
-    sendButton.addEventListener('click', () => {
-        const message = messageInput.value;
-        if (message.trim() !== '') {
-            addMessage(message);
-            messageInput.value = '';
-        }
-    });
-
-    // Event listener for pressing Enter key to send a message
-    messageInput.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            const message = messageInput.value;
-            if (message.trim() !== '') {
-                addMessage(message);
-                messageInput.value = '';
+        showMessageInputButton.addEventListener("click", function () {
+            if (messageInput.style.display === "none" || messageInput.style.display === "") {
+                messageInput.style.display = "block";
+            } else {
+                messageInput.style.display = "none";
             }
-        }
-    });
-
-    // Initial welcome message
-    addMessage('Welcome to the chat!');
+        });
+    }
 });
+// Get the "30DAy Backend" link by its id
+const targetId = document.getElementById("one");
+
+// Add a click event listener to the link 
+webTrainingLink.addEventListener("click", function (event) {
+  // Prevent the default link behavior (to avoid reloading the current page)
+  event.preventDefault();
+
+  // Redirect to the index.html page
+  window.location.href = "one.html";
+});
+
+
+// Get the "job offers" link by its id
+
+
+// Add a click event listener to the link
+webTrainingLink.addEventListener("click", function (event) {
+  // Prevent the default link behavior (to avoid reloading the current page)
+  event.preventDefault();
+
+  // Redirect to the index.html page
+  window.location.href = "index.html";
+});
+// Get the "tc Conference" link by its id
+const tcConferenceLink = document.getElementById("two");
+
+// Add a click event listener to the link
+tcConferenceLink.addEventListener("click", function (event) {
+  // Prevent the default link behavior (to avoid reloading the current page)
+  event.preventDefault();
+
+  // Redirect to the conference.html page
+  window.location.href = "two.html";
+});
+// Get the "" link by its id
+const foor = document.getElementById("three");
+
+// Add a click event listener to the link
+foor.addEventListener("click", function (event) {
+  // Prevent the default link behavior (to avoid reloading the current page)
+  event.preventDefault();
+
+  // Redirect to the conference.html page
+  window.location.href = "three.html";
+});
+
+// Get the "innoclickHuck" link by its id
+const innoclickHuck = document.getElementById("foor");
+
+// Add a click event listener to the link
+innoclickHuck.addEventListener("click", function (event) {
+  // Prevent the default link behavior (to avoid reloading the current page)
+  event.preventDefault();
+
+  // Redirect to the conference.html page
+  window.location.href = "three.html";
+});
+
+// Get the "iPhone15Link" link by its id
+const iPhone15Link = document.getElementById("five");
+
+// Add a click event listener to the link
+iPhone15Link.addEventListener("click", function (event) {
+  // Prevent the default link behavior (to avoid reloading the current page)
+  event.preventDefault();
+
+  // Redirect to the phone.html page
+  window.location.href = "foor.html";
+});
+
